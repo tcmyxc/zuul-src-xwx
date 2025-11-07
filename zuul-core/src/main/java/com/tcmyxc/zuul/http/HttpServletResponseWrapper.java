3fc081acab3f461704d55797ba2387d8fb263011
tcmyxc <1282494272@qq.com>
2025-11-07 16:40:11 +0800

    Create HttpServletResponseWrapper.java

---
+++ zuul-core/src/main/java/com/tcmyxc/zuul/http/HttpServletResponseWrapper.java
@@ -0,0 +1,29 @@
+package com.tcmyxc.zuul.http;
+
+
+import javax.servlet.http.HttpServletResponse;
+
+public class HttpServletResponseWrapper extends javax.servlet.http.HttpServletResponseWrapper {
+
+    private int status = 0;
+
+    public HttpServletResponseWrapper(HttpServletResponse response) {
+        super(response);
+    }
+
+    @Override
+    public void setStatus(int sc) {
+        this.status = sc;
+        super.setStatus(sc);
+    }
+
+    @Override
+    public void setStatus(int sc, String sm) {
+        this.status = sc;
+        super.setStatus(sc, sm);
+    }
+
+    public int getStatus(){
+        return status;
+    }
+}
