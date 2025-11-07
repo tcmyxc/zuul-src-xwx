19da2f8fcae26d007d6b945304cb467892128a44
tcmyxc <1282494272@qq.com>
2025-11-07 16:40:09 +0800

    Update FilterProcessor.java

--- zuul-core/src/main/java/com/tcmyxc/zuul/FilterProcessor.java
+++ zuul-core/src/main/java/com/tcmyxc/zuul/FilterProcessor.java
@@ -4,8 +4,11 @@ package com.tcmyxc.zuul;
 import com.netflix.servo.monitor.DynamicCounter;
 import com.tcmyxc.zuul.context.Debug;
 import com.tcmyxc.zuul.context.RequestContext;
+import com.tcmyxc.zuul.exception.ZuulException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 /**
  * 执行过滤器的核心类
@@ -41,6 +44,79 @@ public class FilterProcessor {
         }
 
         boolean result = false;
+        List<ZuulFilter> list = FilterLoader.getInstance().getFiltersByType(filterType);
+        if(list != null){
+            for (int i = 0; i < list.size(); i++) {
+                ZuulFilter filter = list.get(i);
+                Object processedResult = processZuulFilter(filter);
+                if(processedResult != null && processedResult instanceof Boolean){
+                    result |= (Boolean) processedResult;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public Object processZuulFilter(ZuulFilter filter) throws ZuulException{
+        RequestContext ctx = RequestContext.getCurrentContext();
+        boolean debugged = ctx.debugRouting();
+        final String metricPrefix = "zuul.filter-";
+        long execTime = 0;
+        String filterName = "";
+        try {
+            long startTime = System.currentTimeMillis();
+            filterName = filter.getClass().getSimpleName();
+
+            RequestContext copy = null;
+            Object o = null;
+            Throwable th = null;
+
+            if(debugged){
+                Debug.addRoutingDebug("Filter " + filter.filterType() + " " + filter.filterOrder() + " " + filterName);
+                copy = ctx.copy();
+            }
+
+            ZuulFilterResult result = filter.runFilter();
+            ExecutionStatus status = result.getStatus();
+            execTime = System.currentTimeMillis() - startTime;
+
+            switch (status){
+                case FAILED:
+                    th = result.getException();
+                    ctx.addFilterExecutionSummary(filterName, ExecutionStatus.FAILED.name(), execTime);
+                    break;
+                case SUCCESS:
+                    o = result.getResult();
+                    ctx.addFilterExecutionSummary(filterName, ExecutionStatus.SUCCESS.name(), execTime);
+                    if(debugged){
+                        Debug.addRoutingDebug("Filter {" + filterName + " TYPE:" + filter.filterType() + " ORDER:" + filter.filterOrder() + "} Execution time = " + execTime + "ms");
+                        Debug.compareContextState(filterName, copy);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            if(th != null){
+                throw th;
+            }
+            usageNotifier.notify(filter, status);
+            return o;
+        } catch (Throwable e) {
+            if(debugged){
+                Debug.addRoutingDebug("Running Filter failed " + filterName + " type:" + filter.filterType() + " order:" + filter.filterOrder() + " " + e.getMessage());
+            }
+            usageNotifier.notify(filter, ExecutionStatus.FAILED);
+            if(e instanceof ZuulException){
+                throw (ZuulException) e;
+            }
+            else {
+                ZuulException ex = new ZuulException(e, "Filter threw Exception", 500, filter.filterType() + ":" + filterName);
+                ctx.addFilterExecutionSummary(filterName, ExecutionStatus.FAILED.name(), execTime);
+                throw ex;
+            }
+        }
     }
 
     public static class BasicFilterUsageNotifier implements FilterUsageNotifier{
