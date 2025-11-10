c39379528056a66434468c5dc447a118f0a4187f
tcmyxc <1282494272@qq.com>
2025-11-10 16:05:19 +0800

    删除cloud相关

--- spring-cloud-netflix-zuul/src/main/java/com/tcmyxc/zuul/cloud/filters/discovery/SimpleServiceRouteMapper.java
+++
@@ -1,8 +0,0 @@
-package com.tcmyxc.zuul.cloud.filters.discovery;
-
-public class SimpleServiceRouteMapper implements ServiceRouteMapper{
-    @Override
-    public String apply(String serviceId) {
-        return serviceId;
-    }
-}
